4c4f067f6edf83bf204065d320f9c8afba8af881
HananSatar <41490988+HananSatar@users.noreply.github.com>
2018-08-01 16:22:04 +0300

    Add files via upload

---
+++ Q1_list.py
@@ -0,0 +1,2 @@
+s = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+s1 = [print(pow(num, 2)) for num in s]
---
+++ Q2_list.py
@@ -0,0 +1,4 @@
+
+L1 = [10, 15, 20, 12, 25, 45, 12]
+L2 = [4, 5, 6, 3, 2, 9]
+s = [print(num2, " is factor ", num1) for num1 in L1 for num2 in L2 if num1 % num2 == 0]
